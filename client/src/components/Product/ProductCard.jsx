4549f47069b88d04204739591517a96cf9a4b00d
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-09 21:24:53 +0300

    Product

--- client/src/components/Product/ProductCard.jsx
+++ client/src/components/Product/ProductCard.jsx
@@ -4,14 +4,17 @@ import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./../Product/Product.module.css";
 
 function ProductCard({ product }) {
-  const { image, title, id, rating, price } = product;
+  const { image, title, rating, price } = product;
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <>
       <div className={classes.card__container}>
         <a href="">
           <img src={image} alt="" />
           <div>
-            <h3>{title}</h3>
+            <h3>{truncate(title, 45)}</h3>
             <div className={classes.rating}>
               {/* rating */}
               <Rating value={rating?.rate} precision={0.1} />
