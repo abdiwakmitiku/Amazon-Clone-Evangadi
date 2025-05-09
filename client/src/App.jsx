dc147d9bda1bd81f7a790b124131c0a843d83653
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-09 20:13:55 +0300

    Product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import Header from "./components/Header/Header"; 
+import "./App.css";
+import Header from "./components/Header/Header";
 import CarouselEffect from "./components/Carousel/CarouselEffect";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Category from "./components/Category/Category";
+import Product from "./components/Product/Product";
 
 function App() {
   return (
     <>
       <Header />
       <CarouselEffect />
-      <Category/>
+      <Category />
+      <Product />
     </>
   );
 }
