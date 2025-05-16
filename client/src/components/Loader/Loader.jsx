cde96a06eb3f2ec26094c8355558473723ce8ad2
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-16 17:21:12 +0300

    Loader

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScaleLoader } from "react-spinners";
+
+function Loader() {
+  return (
+    <div>
+      <ScaleLoader color="rgba(54, 215, 183, 1)" />
+    </div>
+  );
+}
+
+export default Loader;
