63b8e445b10d40e67681f81f6a5ed5beea50d9bf
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-17 12:01:48 +0300

    AddToCartFunctionality

---
+++ client/src/DataProvider/DataProvider.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useReducer } from "react";
+import { initialState, reducer } from "../Utility/reducer";
+
+export const DataContext = createContext();
+
+const DataProvider = ({ children, reducer, initialState }) => {
+  return (
+    <DataContext.Provider value={useReducer(reducer, initialState)}>
+      {children}
+    </DataContext.Provider>
+  );
+};
