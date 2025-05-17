31fc45f3e90bae57ff22182568db8de4970d9764
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-17 20:20:22 +0300

    test

--- client/src/Pages/Cart/Cart.jsx
+++ client/src/Pages/Cart/Cart.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import LayOut from "../../components/Layout/LayOut";
+import { DataContext } from "../../components/DataProvider/DataProvider";
 
 function Cart() {
+   const [{basket, user}, dispatch]=useContext( DataContext)
   return (
     <LayOut>
-      <div>Cart</div>
+      <section>
+        <div>
+          <h2>Hello</h2>
+          <h3>Your Shopping Basket</h3>
+          <hr />
+          {/* {
+            basket?.length==0?
+          } */}
+        </div>
+        <div></div>
+      </section>
     </LayOut>
   );
 }
