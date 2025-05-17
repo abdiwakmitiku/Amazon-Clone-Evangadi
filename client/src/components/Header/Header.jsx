9134fe83bbcc92e39fb6092f3c84f16c05c163c6
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-17 12:51:50 +0300

    HeaderSticky

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   const [{basket}, dispatch] = useContext(DataContext);
 
   return (
-    <>
+    <section className={classes.fixed}>
       <section>
         {" "}
         <div className={classes.header__container}>
@@ -74,7 +74,7 @@ function Header() {
         </div>
       </section>
       <LowerHeader />
-    </>
+    </section>
   );
 }
 
