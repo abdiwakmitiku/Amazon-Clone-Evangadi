4cf81f97afe8f9ec2f44876bf986a11d672637b3
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-05-18 21:51:50 +0300

    CartPage

--- client/src/Pages/Payment/Payment.jsx
+++ client/src/Pages/Payment/Payment.jsx
@@ -5,7 +5,20 @@ import LayOut from "../../components/Layout/LayOut";
 function Payment() {
   return (
     <LayOut>
-      <div>Payment</div>
+      <section>
+          <h1 style={{ padding: "30px" }}>Results</h1>
+          <p style={{ padding: "30px" }}>Category/{categoryName}</p>
+          <hr />
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div className={classes.products_container}>
+              {Results?.map((product) => (
+                <ProductCard key={product.id} product={product} renderDesc={false} renderAdd={true} />
+              ))}
+            </div>
+          )}
+        </section>
     </LayOut>
   );
 }
